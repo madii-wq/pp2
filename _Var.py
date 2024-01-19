6d1e4af006c9b8c19e9e6a5c897e53296851fbb4
Madiyar <madiaraza2002@gmail.com>
2024-01-19 13:27:35 +0500

    Variables

---
+++ _Var.py
@@ -0,0 +1,21 @@
+#EX 1
+carname= "Volvo"
+#Ex 2
+x = 50
+#EX 3
+x = 5
+y = 10
+print(x+y)
+#EX 4
+x = 5
+y = 10
+z = x + y
+print(z)
+#EX 5
+x,y,z = "Orange", "Banana", "Cherry
+#EX 6
+x=y=z="Orange"
+#EX 7
+def myfunc():
+  global x
+  x = "fantastic"
