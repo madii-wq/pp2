578ab1a6318c2080b2f9b52a74cf03d7860d8169
Madiyar <madiaraza2002@gmail.com>
2024-01-22 12:02:11 +0500

    operators

---
+++ _Operators.py
@@ -0,0 +1,14 @@
+#EX1
+print(10*5)
+#EX2
+print(10/2)
+#EX3
+fruits = ["apple", "banana"]
+if "apple" in fruits:
+  print("Yes, apple is a fruit!")
+#EX4
+if 5!=10:
+  print("5 and 10 is not equal")
+#EX5
+if 5 == 10 or 4 == 4:
+  print("At least one of the statements is true")
