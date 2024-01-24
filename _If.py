c9eca05d4f7762123afbf5238e807659364fd083
Madiyar <madiaraza2002@gmail.com>
2024-01-24 14:23:24 +0500

    if...else

---
+++ _If.py
@@ -0,0 +1,46 @@
+#EX1
+a = 50
+b = 10
+if a>b:
+ print("Hello World")
+#EX2
+a = 50
+b = 10
+if a!=b:
+  print("Hello World")
+#EX3
+a = 50
+b = 10
+if a==b:
+  print("Yes")
+else:
+  print("No")
+#EX4
+a = 50
+b = 10
+if a==b:
+  print("1")
+elif a>b:
+  print("2")
+else:
+  print("3")
+#EX5
+c = d = 20
+if a == b and c == d:
+  print("Hello")
+#EX6
+if a == b or c == d:
+  print("Hello")
+#EX7
+if 5 > 2:
+    print("YES")
+#EX8
+a = 2
+b = 5
+print("YES") if a == b else print("NO")
+#EX9
+a = 2
+b = 50
+c = 2
+if a == c or b == c:
+ print("YES")
